07a187f997bdec7fb1aeddf81b26c06ed1217984
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2023-07-10 22:24:33 +0200

    Update nmap2urls.py
    remove need for multiple clients

--- nmap2urls.py
+++ nmap2urls.py
@@ -10,7 +10,7 @@ async def extract_http_urls_from_nmap_xml(path):
     report = NmapParser.parse_fromfile(nmap_file)
     urls = []
 
-    async with httpx.AsyncClient(verify=False) as client:
+    async with httpx.AsyncClient(verify=False, timeout=httpx.Timeout(30.0)) as client:
         tasks = []
         for host in report.hosts:
             for service in host.services:
@@ -25,27 +25,26 @@ async def extract_http_urls_from_nmap_xml(path):
                     if "http" in service.service:
                         urls.append(line)
                     else:
-                        tasks.append(probe_url(line, urls))
+                        tasks.append(probe_url(client, line, urls))
 
         await asyncio.gather(*tasks)
 
     for url in list(dict.fromkeys(urls)):
         print(url)
 
-async def probe_url(url, urls):
+async def probe_url(client, url, urls):
     try:
-        async with httpx.AsyncClient(verify=False) as client:
-            r = await client.get(url, timeout=60)
-            urls.append(url)
+        r = await client.get(url)
+        urls.append(url)
     except:
         try:
             if url.startswith('http://'):
                 url = url.replace("http://", "https://")
             elif url.startswith('https://'):
                 url = url.replace("https://", "http://")
-            async with httpx.AsyncClient(verify=False) as client:
-                r = await client.get(url, timeout=60)
-                urls.append(url)
+           
+            r = await client.get(url)
+            urls.append(url)
         except:
             pass
 
